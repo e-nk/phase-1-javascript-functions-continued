9eaba34c5a44619839c3642c8303caafac69d1e6
e-nk <enocklagat747@gmail.com>
2022-11-26 09:28:08 +0300

    DONE WITH ASSIGNMENT

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    return (`This Saturday, I want to ${activity}!`);
+}
+  
+function mondayWork (activity='go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+  
+function wrapAdjective (asteri='*'){
+    return function worker(description='a hard worker'){
+      return `You are ${asteri}${description}${asteri}!`;
+    }
+}
